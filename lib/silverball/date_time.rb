fd8d05d5f874cd669c6cd0d05e2bcccf823fd5f8
Michael Senn <michael@senn.email>
2017-01-03 07:45:01 +0100

    Extends `timespan_in_words` to support using one unit only.

--- lib/silverball/date_time.rb
+++ lib/silverball/date_time.rb
@@ -6,26 +6,60 @@ module Silverball
     SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60
     SECONDS_PER_DAY = SECONDS_PER_HOUR * 24
 
-    def timespan_in_words(seconds)
-      if seconds >= SECONDS_PER_DAY
+    def timespan_in_words(seconds, unit: nil, round: 2)
+      seconds = seconds.to_f unless unit.nil?
+
+      if seconds >= SECONDS_PER_DAY && (unit.nil? || unit == :days)
         days = seconds / SECONDS_PER_DAY
         seconds -= days * SECONDS_PER_DAY
+
+        if unit == :days
+          if days == days.to_i
+            days = days.to_i
+          else
+            days - days.round(round)
+          end
+        end
       else
         days = 0
       end
 
-      if seconds >= SECONDS_PER_HOUR
+      if seconds >= SECONDS_PER_HOUR && (unit.nil? || unit == :hours)
         hours = seconds / SECONDS_PER_HOUR
         seconds -= hours * SECONDS_PER_HOUR
+
+        if unit == :hours
+          if hours == hours.to_i
+            hours = hours.to_i
+          else
+            hours - hours.round(round)
+          end
+        end
       else
         hours = 0
       end
 
-      if seconds >= SECONDS_PER_MINUTE
+      if seconds >= SECONDS_PER_MINUTE && (unit.nil? || unit == :minutes)
         minutes = seconds / SECONDS_PER_MINUTE
         seconds -= minutes * SECONDS_PER_MINUTE
+
+        if unit == :minutes
+          if minutes == minutes.to_i
+            minutes = minutes.to_i
+          else
+            minutes - minutes.round(round)
+          end
+        end
       else
         minutes = 0
+      end
+
+      if unit == :seconds
+        if seconds == seconds.to_i
+          seconds = seconds.to_i
+        else
+          seconds = seconds.round(rouond)
+        end
       end
 
       out = []
--- spec/silverball/date_time_spec.rb
+++ spec/silverball/date_time_spec.rb
@@ -11,32 +11,52 @@ module Silverball
     end
 
     describe '::timespan_in_words' do
-      it 'should handle a timespan of 0 seconds' do
-        expect(subject.timespan_in_words(0)).to eq '0s'
+      context 'with full output' do
+        it 'should handle a timespan of 0 seconds' do
+          expect(subject.timespan_in_words(0)).to eq '0s'
+        end
+
+        it 'should handle sub-minute timespans' do
+          expect(subject.timespan_in_words(45)).to eq '45s'
+        end
+
+        it 'should handle timespans equal to one minute' do
+          expect(subject.timespan_in_words(60)).to eq '1m'
+        end
+
+        it 'should handle timespans above one minute' do
+          expect(subject.timespan_in_words(145)).to eq '2m 25s'
+        end
+
+        it 'should handle timespans equal to one hour' do
+          expect(subject.timespan_in_words(2 * 60 * 60)).to eq '2h'
+        end
+
+        it 'should handle timespans above one hour' do
+          expect(subject.timespan_in_words(4 * 60 * 60 + 4 * 60 + 4)).to eq '4h 4m 4s'
+        end
+
+        it 'should handle timespans above or equal to one day' do
+          expect(subject.timespan_in_words(3 * 24 * 60 * 60 + 2 * 60 * 60 + 20)).to eq '3d 2h 20s'
+        end
       end
 
-      it 'should handle sub-minute timespans' do
-        expect(subject.timespan_in_words(45)).to eq '45s'
-      end
+      context 'with single-type output' do
+        it 'should support seconds only' do
+          expect(subject.timespan_in_words(240, unit: :seconds)).to eq '240s'
+        end
 
-      it 'should handle timespans equal to one minute' do
-        expect(subject.timespan_in_words(60)).to eq '1m'
-      end
+        it 'should support minutes only' do
+          expect(subject.timespan_in_words(270, unit: :minutes)).to eq '4.5m'
+        end
 
-      it 'should handle timespans above one minute' do
-        expect(subject.timespan_in_words(145)).to eq '2m 25s'
-      end
+        it 'should support hours only' do
+          expect(subject.timespan_in_words(3 * 60 * 60 + 6 * 60, unit: :hours)).to eq '3.1h'
+        end
 
-      it 'should handle timespans equal to one hour' do
-        expect(subject.timespan_in_words(2 * 60 * 60)).to eq '2h'
-      end
-
-      it 'should handle timespans above one hour' do
-        expect(subject.timespan_in_words(4 * 60 * 60 + 4 * 60 + 4)).to eq '4h 4m 4s'
-      end
-
-      it 'should handle timespans above or equal to one day' do
-        expect(subject.timespan_in_words(3 * 24 * 60 * 60 + 2 * 60 * 60 + 20)).to eq '3d 2h 20s'
+        it 'should support days only' do
+          expect(subject.timespan_in_words(7 * 24 * 60 * 60 + 12 * 60 * 60, unit: :days)).to eq '7.5d'
+        end
       end
     end
 
